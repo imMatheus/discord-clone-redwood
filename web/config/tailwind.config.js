e5e0a9847d4b7973ad15a95918b00c6a2c91d6e5
imMatheus <matheusmendesbarata03@gmail.com>
2022-10-02 16:28:47 +0200

    added auth

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'clr-bg': '#333',
+      },
+    },
   },
   plugins: [],
 }
